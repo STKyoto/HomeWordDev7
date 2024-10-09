41fe18ac20879fa391f05f1298fd217118cfb756
Valentyn <mazurv916@gmail.com>
2024-10-09 17:19:25 +0200

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,6 @@ import org.example.db.Database;
 
 public class Main {
     public static void main(String[] args) {
-        Database database = new Database();
-        ClientService clientService = new ClientService();
-//        System.out.println(clientService.create("Monika"));
-//        System.out.println(clientService.getById(2));
-//        clientService.setName(13, "Viktor");
-//        clientService.deleteById(14);
-        clientService.listAll().stream().forEach(System.out::println);
+
     }
 }
